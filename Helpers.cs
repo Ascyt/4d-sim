a8e6c2301d2fdcb8bae3f3ec054d09c5353b217d
Ascyt <account@ascyt.com>
2025-02-28 12:21:45 +0100

    Added GetPerspectiveViewingTransformMatrix function

--- Helpers.cs
+++ Helpers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,27 +6,57 @@ using UnityEngine;
 public static class Helpers
 {
     // Thanks to https://hollasch.github.io/ray4/Four-Space_Visualization_of_4D_Objects.html#chapter2
-    public static Vector4 Cross(this Vector4 v, Vector4 a, Vector4 b)
+    public static Vector4 Cross4(Vector4 a, Vector4 b, Vector4 c)
     {
         float A, B, C, D, E, F; // Intermediate Values
 
         // Calculate intermediate values
-        A = (a.x * b.y) - (a.y * b.x);
-        B = (a.x * b.z) - (a.z * b.x);
-        C = (a.x * b.w) - (a.w * b.x);
-        D = (a.y * b.z) - (a.z * b.y);
-        E = (a.y * b.w) - (a.w * b.y);
-        F = (a.z * b.w) - (a.w * b.z);
-
+        A = (b.x * c.y) - (b.y * c.x);
+        B = (b.x * c.z) - (b.z * c.x);
+        C = (b.x * c.w) - (b.w * c.x);
+        D = (b.y * c.z) - (b.z * c.y);
+        E = (b.y * c.w) - (b.w * c.y);
+        F = (b.z * c.w) - (b.w * c.z);
 
         // Calculate the result-vector components
         Vector4 result = new Vector4(
-            (v.y * F) - (v.z * E) + (v.w * D),
-            -(v.x * F) + (v.z * C) - (v.w * B),
-            (v.x * E) - (v.y * C) + (v.w * A),
-            -(v.x * D) + (v.y * B) - (v.z * A)
+            (a.y * F) - (a.z * E) + (a.w * D),
+            -(a.x * F) + (a.z * C) - (a.w * B),
+            (a.x * E) - (a.y * C) + (a.w * A),
+            -(a.x * D) + (a.y * B) - (a.z * A)
             );
 
         return result;
+    }
+
+    // Thanks to https://hollasch.github.io/ray4/Four-Space_Visualization_of_4D_Objects.html#chapter4
+    public static void GetPerspectiveViewingTransformMatrix(Vector4 from, Vector4 to, Vector4 up, Vector4 over, 
+        out Vector4 wa, out Vector4 wb, out Vector4 wc, out Vector4 wd)
+    {
+        float norm;
+
+        // Get the normalized wd column-vector
+        wd = to - from;
+        norm = wd.magnitude;
+        if (norm == 0)
+            throw new InvalidOperationException("To point and From point are the same.");
+        wd *= 1f / norm;
+
+        // Calculate the normalized wa column-vector
+        wa = Cross4(up, over, wd);
+        norm = wa.magnitude;
+        if (norm == 0)
+            throw new InvalidOperationException("Invalid Up vector.");
+        wa *= 1f / norm;
+
+        // Calculate the normalized wb column-vector
+        wb = Cross4(over, wd, wa);
+        norm = wb.magnitude;
+        if (norm == 0)
+            throw new InvalidOperationException("Invalid Over vector.");
+        wb *= 1f / norm;
+
+        // Calculate the wc column-vector
+        wc = Cross4(wd, wa, wb);
     }
 }
