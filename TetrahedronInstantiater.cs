55de05c6695414007877754edc61441215f8aecb
Ascyt <account@ascyt.com>
2025-02-28 11:16:22 +0100

    Rendering fixes to 3D view

--- TetrahedronInstantiater.cs
+++ TetrahedronInstantiater.cs
@@ -19,20 +19,26 @@ public class TetrahedronInstantiater : MonoBehaviour
         Tetrahedron[] t = new Tesseract().hypermesh;
         for (int i = 0; i < t.Length; i++)
         {
-            InstantiateTetrahedron(t[i], new Vector3(t[i].vertices.Select(v => v.w * 10f).Average(), 0, 0));
+            Vector3 delta = new Vector3(t[i].vertices.Select(v => v.w).Average(), 0, 0);
+            InstantiateTetrahedron(t[i], delta * 0f);
         }
     }
 
     private int[] triangles = new int[]
     {
-        0, 2, 1, // First triangle
-        0, 3, 2, // Second triangle
-        0, 1, 3, // Third triangle
-        1, 2, 3  // Fourth triangle
+        0, 1, 2,
+        0, 3, 1,
+        1, 3, 2,
+        2, 3, 0
     };
 
     public GameObject InstantiateTetrahedron(Tetrahedron tetrahedron, Vector3 position)
     {
+        // TODO: Use perspective projection https://hollasch.github.io/ray4/Four-Space_Visualization_of_4D_Objects.html#chapter4
+        Vector3[] vertices = tetrahedron.vertices.Select(v => new Vector3(v.x, v.y, v.z)).ToArray();
+
+        Vector3[] uniqueVertices = GetUniqueVertices(vertices);
+
         GameObject newGameObject = new GameObject($"Tetrahedron");
 
         newGameObject.transform.position = position;
@@ -41,13 +47,63 @@ public class TetrahedronInstantiater : MonoBehaviour
         MeshRenderer meshRenderer = newGameObject.AddComponent<MeshRenderer>();
 
         Mesh mesh = new Mesh();
-        mesh.vertices = tetrahedron.vertices.Select(v => new Vector3(v.x, v.y, v.z)).ToArray();
-        mesh.triangles = triangles;
+        mesh.vertices = vertices;
 
+
+        if (uniqueVertices.Length == 3)
+        {
+            // Create a triangle
+            mesh.triangles = new int[] { 0, 1, 2 };
+        }
+        else
+        {
+            mesh.triangles = triangles;
+        }
+
+        mesh.RecalculateNormals();
+        /*
+        // Check if normals are facing upwards, flip if necessary
+        if (Vector3.Dot(mesh.normals[0], Vector3.up) < 0)
+        {
+            FlipTriangles(mesh);
+        }
+
+        mesh.RecalculateBounds();
+        */
         meshFilter.mesh = mesh;
         meshRenderer.material = new Material(tetrahedronMaterial);
-        meshRenderer.material.color = Color.black;
+        meshRenderer.material.color = Color.white;
 
         return newGameObject;
+    }
+
+    private static Vector3[] GetUniqueVertices(Vector3[] verts)
+    {
+        // Use a list to store unique vertices
+        List<Vector3> uniqueVerts = new List<Vector3>();
+
+        foreach (var vert in verts)
+        {
+            if (!uniqueVerts.Contains(vert))
+            {
+                uniqueVerts.Add(vert);
+            }
+        }
+
+        return uniqueVerts.ToArray();
+    }
+
+    private static void FlipTriangles(Mesh mesh)
+    {
+        int[] triangles = mesh.triangles;
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            // Swap two indices to reverse winding order
+            int temp = triangles[i];
+            triangles[i] = triangles[i + 1];
+            triangles[i + 1] = temp;
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
