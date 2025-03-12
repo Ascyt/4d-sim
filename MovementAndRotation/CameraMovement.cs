b4cbab6fdbb287f0e82a2787df8deb649a44c73b
Ascyt <account@ascyt.com>
2025-03-12 13:39:10 +0100

    Movement and scene improvements

--- MovementAndRotation/CameraMovement.cs
+++ MovementAndRotation/CameraMovement.cs
@@ -32,49 +32,55 @@ public class CameraMovement : MonoBehaviour
         float speed = movementSpeed * Time.deltaTime;
         bool positionUpdated = false;
 
+        Vector4 delta = Vector4.zero;
+
         if (Input.GetKey(MoveRight))
         {
-            cameraPos.position += new Vector4(speed, 0, 0, 0);
+            delta += new Vector4(speed, 0, 0, 0);
             positionUpdated = true;
         }
         if (Input.GetKey(MoveLeft))
         {
-            cameraPos.position += new Vector4(-speed, 0, 0, 0);
+            delta += new Vector4(-speed, 0, 0, 0);
             positionUpdated = true;
         }
 
         if (Input.GetKey(MoveUp))
         {
-            cameraPos.position += new Vector4(0, speed, 0, 0);
+            delta += new Vector4(0, speed, 0, 0);
             positionUpdated = true;
         }
         if (Input.GetKey(MoveDown))
         {
-            cameraPos.position += new Vector4(0, -speed, 0, 0);
+            delta += new Vector4(0, -speed, 0, 0);
             positionUpdated = true;
         }
 
         if (Input.GetKey(MoveForwards))
         {
-            cameraPos.position += new Vector4(0, 0, speed, 0);
+            delta += new Vector4(0, 0, speed, 0);
             positionUpdated = true;
         }
         if (Input.GetKey(MoveBackwards))
         {
-            cameraPos.position += new Vector4(0, 0, -speed, 0);
+            delta += new Vector4(0, 0, -speed, 0);
             positionUpdated = true;
         }
 
         if (Input.GetKey(MoveAna))
         {
-            cameraPos.position += new Vector4(0, 0, 0, speed);
+            delta += new Vector4(0, 0, 0, speed);
             positionUpdated = true;
         }
         if (Input.GetKey(MoveKata))
         {
-            cameraPos.position += new Vector4(0, 0, 0, -speed);
+            delta += new Vector4(0, 0, 0, -speed);
             positionUpdated = true;
         }
+
+        delta = delta.Rotate(cameraPos.rotation);
+
+        cameraPos.position += delta;
 
         if (positionUpdated)
         {
