e5443dedb3f0378bd03bef42b40c0421982273aa
Ascyt <account@ascyt.com>
2025-03-13 13:55:15 +0100

    Reverted last change

--- MovementAndRotation/CameraMovement.cs
+++ MovementAndRotation/CameraMovement.cs
@@ -6,8 +6,6 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField]
     public float movementSpeed;
-    [SerializeField]
-    public bool heightIsIndependentOfRotation;
 
     private const bool USE_DVORAK = false;
     private static KeyCode MoveRight        = USE_DVORAK ? KeyCode.E : KeyCode.D;
@@ -49,20 +47,12 @@ public class CameraMovement : MonoBehaviour
 
         if (Input.GetKey(MoveUp))
         {
-            if (heightIsIndependentOfRotation)
-                cameraPos.position += new Vector4(0, speed, 0, 0);
-            else
-                delta += new Vector4(0, speed, 0, 0);
-
+            delta += new Vector4(0, speed, 0, 0);
             positionUpdated = true;
         }
         if (Input.GetKey(MoveDown))
         {
-            if (heightIsIndependentOfRotation)
-                cameraPos.position += new Vector4(0, -speed, 0, 0);
-            else
-                delta += new Vector4(0, -speed, 0, 0);
-
+            delta += new Vector4(0, -speed, 0, 0);
             positionUpdated = true;
         }
 
